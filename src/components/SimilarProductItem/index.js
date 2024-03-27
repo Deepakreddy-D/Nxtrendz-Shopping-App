c6672f22b6f8b8db719d31f1c7234f0420913e1a
Deepak Reddy D <114271318+Deepakreddy-D@users.noreply.github.com>
2024-03-27 06:25:47 +0530

    Link added to the similarProducts

--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -1,30 +1,37 @@
 import './index.css'
+import {Link} from 'react-router-dom'
 
 const SimilarProductItem = props => {
   const {productDetails} = props
-  const {title, brand, imageUrl, rating, price} = productDetails
+
+  const {title, brand, imageUrl, rating, price, id} = productDetails
+  //   const {id} = useParams()
+  //   const {match} = props
+  //   const {id} = match.params
 
   return (
-    <li className="similar-product-item">
-      <img
-        src={imageUrl}
-        className="similar-product-image"
-        alt={`similar product ${title}`}
-      />
-      <p className="similar-product-title">{title}</p>
-      <p className="similar-products-brand">by {brand}</p>
-      <div className="similar-product-price-rating-container">
-        <p className="similar-product-price">Rs {price}/-</p>
-        <div className="similar-product-rating-container">
-          <p className="similar-product-rating">{rating}</p>
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/star-img.png"
-            alt="star"
-            className="similar-product-star"
-          />
+    <Link to={`/products/${id}`} className="link-item">
+      <li className="similar-product-item">
+        <img
+          src={imageUrl}
+          className="similar-product-img"
+          alt={`similar product ${title}`}
+        />
+        <p className="similar-product-title">{title}</p>
+        <p className="similar-products-brand">by {brand}</p>
+        <div className="similar-product-price-rating-container">
+          <p className="similar-product-price">Rs {price}/-</p>
+          <div className="similar-product-rating-container">
+            <p className="similar-product-rating">{rating}</p>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+              alt="star"
+              className="similar-product-star"
+            />
+          </div>
         </div>
-      </div>
-    </li>
+      </li>
+    </Link>
   )
 }
 
